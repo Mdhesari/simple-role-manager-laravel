b894df415803c44338b2a27bdbdde172f7420868
Mohammad Fazel <mdhesari99@gmail.com>
2020-08-06 14:23:29 +0430

    update exceptions

--- src/Exceptions/RoleGetRoleRepositoryFunctionIsNotFound.php
+++ src/Exceptions/RoleGetRoleRepositoryFunctionIsNotFound.php
@@ -6,5 +6,34 @@ use Exception;
 
 class RoleGetRoleRepositoryFunctionIsNotFound extends Exception
 {
-    //
+    /**
+     * message
+     *
+     * @var string
+     */
+    protected $message = "Please setup a repository for role and follow correct installation instructions.";
+
+    /**
+     * render
+     *
+     * @return mixed
+     */
+    public function render()
+    {
+
+        return response()->json([
+            'message' => $this->message
+        ]);
+    }
+
+    /**
+     * report
+     *
+     * @return string
+     */
+    public function report()
+    {
+
+        return $this->message;
+    }
 }

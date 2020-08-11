898f572f496bb0a199a72fd869559eab382caad2
Mohammad Fazel <mdhesari99@gmail.com>
2020-08-12 01:06:35 +0430

    update test role manager install

--- tests/Unit/InstallRoleManagerPackageTest.php
+++ tests/Unit/InstallRoleManagerPackageTest.php
@@ -13,15 +13,41 @@ class InstallRoleManagerPackageTest extends TestCase
     public function the_install_command_copies_the_config_file()
     {
 
-        if (File::exists($project_config_path = config_path('permissions.php'))) {
-
-            File::delete($project_config_path);
-        }
+        $this->deleteIfExists($project_config_path = config_path('permissions.php'));
 
         $this->assertFalse(File::exists($project_config_path));
 
         Artisan::call('permission:setup');
 
         $this->assertTrue(File::exists($project_config_path));
+    }
+
+    /**
+     * @test
+     */
+    public function the_install_command_copies_package_service_provider()
+    {
+        $this->deleteIfExists($project_service_provider = app_path('/Providers/RoleManagerServiceProvider.php'));
+
+        $this->assertFalse(File::exists($project_service_provider));
+
+        Artisan::call('permission:setup');
+
+        $this->assertTrue(File::exists($project_service_provider));
+    }
+
+    /**
+     * delete file if exists
+     *
+     * @param  mixed $file
+     * @return void
+     */
+    private function deleteIfExists($file)
+    {
+
+        if (File::exists($file)) {
+
+            File::delete($file);
+        }
     }
 }
